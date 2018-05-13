327d6c6d2e8cfabfb279ed0efac3afb5bdc0e3e6
Kevin Trogant <greenfoxlight.42@gmail.com>
2018-05-13 13:29:21 +0200

    Load single sector stage2 via LBA addressing

--- boot/x86_64/stage1.asm
+++ boot/x86_64/stage1.asm
@@ -1,60 +1,56 @@
 BITS 16
-SECTION .text
+ORG 0x7c00
     ; The Stage1 bootloader loads sector 2 upwards and jumps
     ; to stage2
 stage1:
+    cli
+    ; set ds, es
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
     mov ah, 0x0e
     mov al, 'B' 
     int 0x10
-
-    ; Get drive geometry
-    mov ah, 0x08
-    mov dl, 0x80
-    int 0x13
-    ; dh is number of heads - 1
-    ; cl & 0x3f is sectors per track
-    inc dh
-    mov BYTE [.number_of_heads], dh
-    and cl, 0x3f
-    mov BYTE [.sectors_per_track], cl
     
-    ; Load stage2
-    xor ax, ax
-    mov es, ax
-    mov bx, 0x7e00 
-    mov sp, 3
-; Params: es:bx target address
-;         dx    LBA (1 based)
-.read_sectors:
-    mov ah, 0x02    ; BIOS read sector function
-    mov al, 1       ; Read 1 sector
-    mov ch, 0x00    ; Select cylinder 0
-    mov dh, 0x00    ; Select head 0
-    mov cl, 0x02    ; Start reading from second sector ( i.e.
-                    ; after the boot sector )
+    ; Check if the BIOS supports LBA Addressing
+    mov ah, 0x41
+    mov bx, 0x55aa
     mov dl, 0x80
     int 0x13
-    jnc SHORT .gotostage2
-    dec sp
+    jnc .loadstage2 ; CF if not supported
+    mov ah, 0x0e
+    mov al, 'E'
+    int 0x10
+    jmp $ ; Hang
+    mov sp, 3
+.loadstage2:
+    mov si, dapack 
+    mov ah, 0x42
+    mov dl, 0x80
+    int 0x13
+    jnc .gotostage2
+    sub sp, 1
     cmp sp, 0x00
-    jz .read_sectors
-    jmp .transfer_error
-    ;cmp ah, 0
-    ;jne .transfer_error
+    jnz .loadstage2
+    mov ah, 0x0e
+    mov al, 'D' 
+    int 0x10
+    jmp $
+
 .gotostage2:
     jmp 0x0000:0x7e00
-.transfer_error:
-    dec sp
-    cmp sp, 0
-    jne .read_sectors
-    mov ah, 0x0e
-    mov al, 'E' 
-    int 0x10
-    jmp $ 
 
 ALIGN 4
-.number_of_heads:   db 0
-.sectors_per_track: db 0
+dapack:
+            db 0x10     ; size of structure    
+            db 0
+.blkcnt:    dw 1 
+.dadd:      dw 0x7e00   ; target address (0:0x7e00)
+            dw 0
+.lba:       dd 1        ; load second sector
+            dd 0
+
 
 bootsig:
     resb 510 - $ - stage1
